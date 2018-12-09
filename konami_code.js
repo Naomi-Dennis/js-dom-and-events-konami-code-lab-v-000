ce1cc346b1a098d2b26ab8f5eb352b1d6a9a9d42
Naomi Dennis <njdennis94@gmail.com>
2018-12-09 23:29:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let entered_code = []
+
+
 function init() {
   // Write your JavaScript code inside the init() function
-  document.body.addEventListener("keydown", checkKonami)
-}
-function checkKonami(e){
+  document.body.addEventListener("keydown", (e) =>{
     let currentIndex = entered_code.length
     let currentKey = code[currentIndex]
-   
-    if(e.which == currentKey){
-      entered_code.push(e.which)
+    let capturedKey =  parseInt(e.which || e.detail);
+  
+  console.log(capturedKey)
+    if(  capturedKey == currentKey){
+      entered_code.push(capturedKey)
        if(entered_code.join("") == code.join("")){
-           window.alert("Colonel, this is Snake, can you hear me?")
+           alert("Colonel, this is Snake, can you hear me?")
       }
     }
     else{
-     
       entered_code = []
     }
+  })
 }
+
+
+//init()
